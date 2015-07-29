89bd196fb750b3f58db70ebe79417323ad35f0e0
Ashwin Kumar <ashwin@micropyramid.com>
2015-07-29 18:10:49 +0530

    fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # pietrack documentation build configuration file, created by
-# sphinx-quickstart on Wed Jul 29 17:57:14 2015.
+# sphinx-quickstart on Wed Jul 29 18:10:32 2015.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -34,7 +34,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['ntemplates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -139,7 +139,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['nstatic']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
