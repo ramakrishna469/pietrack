9d9d3ef9621c38ab24cc496336f3647f4b129cd6
druuu <druuu.mail@gmail.com>
2015-08-19 01:04:55 +0530

    added organization option in register

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,7 @@ from piebase.models import User
 
 class RegisterForm(forms.ModelForm):
     confirm_password = forms.CharField(widget = forms.PasswordInput)
+    organization = forms.CharField()
     class Meta:
         model = User
         fields = ['email', 'first_name', 'password', 'username']
