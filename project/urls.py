f04a17bbdc168493475bd9f86c43ab1f082aefe7
ramakrishna <ramakrishna@micropyramid.com>
2015-08-20 13:35:47 +0530

    project_description completed

--- project/urls.py
+++ project/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns=[
 	url(r'^create-project/$',views.create_project,name='create_project'),
 	url(r'^list-of-projects/$', views.list_of_projects, name='list_of_projects'),
+	url(r'^project-description/(?P<pk>[0-9]+)/$', views.project_description, name='project_description'),
 ]
--- project/views.py
+++ project/views.py
@@ -8,12 +8,13 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from piebase.models import Project, User
 import json
-# Create your views here.
+
+
 @login_required
 def create_project(request):
 	template_name = 'create_project.html'
-	if(request.method=="POST"):		
-		organization=Organization.objects.all()[0] #request.user.organization
+	if(request.method=="POST"):
+		organization=request.user.organization
 		form = CreateProjectForm(request.POST,organization=organization)
 		if(form.is_valid()):
 			slug = slugify(request.POST['name'])
@@ -21,12 +22,20 @@ def create_project(request):
 			return HttpResponse(json.dumps({'error':False,'errors':form.errors}), content_type="application/json")
 		else:
 			return HttpResponse(json.dumps({'error':True,'errors':form.errors}), content_type="application/json")
-	return render(request,template_name) 
+	return render(request,template_name)
 
 
 @login_required
 def list_of_projects(request):
 	template_name = 'list_of_projects.html'
 	projects_list=Project.objects.filter(members__email=request.user)
-	dict_items={'projects_list':projects_list, 'user':request.user}
+	dict_items={'projects_list':projects_list}
+	return render(request, template_name, dict_items)
+
+@login_required
+def project_description(request, pk):
+	template_name='project_description.html'
+	project_object=Project.objects.filter(id=pk)
+	project_members=project_object[0].members.all()
+	dict_items={'project_object':project_object[0], 'project_members':project_members}
 	return render(request, template_name, dict_items)
